52ae16ad36e8cb336cef2b73dd832d03b7b8e491
Vivek Arora <118727613+gitvivek14@users.noreply.github.com>
2024-11-06 23:25:17 +0530

    Update index.js

--- index.js
+++ index.js
@@ -30,7 +30,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(
   cors({
-    origin: "https://cw-client.vercel.app",
+    origin: "http://localhost:5173/",
     methods: ["GET", "POST", "DELETE", "PUT"],
     allowedHeaders: [
       "Content-Type",
